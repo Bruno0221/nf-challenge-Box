54c2365bd0770ddc31f31f12115edd1adc4ac791
Bruno <bruno.dill@protonmail.com>
2024-01-22 10:44:09 +0100

    solved

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import StyledBox from "@/components/BoxWithStyledComponents/index.js";
+import styled from "styled-components";
+
+const FlexContainer = styled.section`
+  display: flex;
+`;
 
 export default function HomePage() {
   return (
-    <div>
+    <FlexContainer>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <StyledBox />
+      <StyledBox isBlack />
+    </FlexContainer>
   );
 }
